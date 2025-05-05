5bef49f32f1d80519141ad0ebefce3dc6e6b4d91
Alejandro <pmurillo@charlotte.edu>
2025-05-04 22:56:45 -0400

    feat(models): define User and Todo ORM models with relationship

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+# models.py
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
@@ -6,22 +7,20 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = "user"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(100), nullable=False)
-    age = Column(Integer, nullable=False)
+    id     = Column(Integer, primary_key=True, index=True)
+    name   = Column(String(100), nullable=False)
+    age    = Column(Integer, nullable=False)
     gender = Column(String(1), nullable=False)
 
     todos = relationship("Todo", back_populates="user")
 
 class Todo(Base):
     __tablename__ = "todo"
-
-    id = Column(Integer, primary_key=True, index=True)
-    task = Column(String(256), nullable=False)
-    priority = Column(Integer)
+    id        = Column(Integer, primary_key=True, index=True)
+    task      = Column(String(256), nullable=False)
+    priority  = Column(Integer)
     due_month = Column(String(50))
-    due_year = Column(Integer)
+    due_year  = Column(Integer)
 
     user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
-    user = relationship("User", back_populates="todos")
+    user    = relationship("User", back_populates="todos")
